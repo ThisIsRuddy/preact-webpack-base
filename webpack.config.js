c62d997c2184cceea45153cfb135592927066955
ThisIsRuddy <ThisIsRuddy@gmail.com>
2017-05-25 16:13:50 +0100

    Update: Webpack config for production env- moar compressionzz!!!

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,39 @@
 const path = require('path');
+const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = {
   entry: './src/App.js',
   output: {
     path: path.resolve('dist'),
     filename: 'client.js'
   },
-
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.(js|html)$/,
+      threshold: 10240,
+      minRatio: 0.8
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'server',
+      analyzerHost: '127.0.0.1',
+      analyzerPort: 8888,
+      reportFilename: 'report.html',
+      defaultSizes: 'parsed',
+      openAnalyzer: true,
+      generateStatsFile: false,
+      statsFilename: 'stats.json',
+      statsOptions: null,
+      logLevel: 'info'
+    })
+  ],
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+      {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/}
     ]
   }
 }
